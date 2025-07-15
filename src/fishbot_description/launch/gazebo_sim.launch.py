a8395166f4425e132ab1199311f167f1de048ca3
wcc <1301734385@qq.com>
2025-07-15 19:10:37 +0800

    It is ok.

---
+++ src/fishbot_description/launch/gazebo_sim.launch.py
@@ -0,0 +1,89 @@
+import launch
+import launch.event_handlers
+import launch_ros
+from ament_index_python.packages import get_package_share_directory
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+
+
+
+def generate_launch_description():
+    # 获取默认路径
+    robot_name_in_model = "fishbot"
+    urdf_tutorial_path = get_package_share_directory('fishbot_description')
+    default_model_path = urdf_tutorial_path + '/urdf/fishbot/fishbot.urdf.xacro'
+    default_world_path = urdf_tutorial_path + '/world/custom_room.world'
+    #配置好的rviz文件（插件等）
+    default_rviz_config_path = urdf_tutorial_path + '/config/rviz/display_model.rviz'
+    # 为launch声明参数
+    action_declare_arg_mode_path = launch.actions.DeclareLaunchArgument(
+        name = 'model', default_value = str(default_model_path), description = 'Xacro的绝对路径' 
+    )
+    # 获取文件内容生成新的参数
+    robot_description = launch_ros.parameter_descriptions.ParameterValue(
+        launch.substitutions.Command(
+            ['xacro ', launch.substitutions.LaunchConfiguration('model')]),
+        value_type=str)
+    # 状态发布节点
+    robot_state_publisher_node = launch_ros.actions.Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description':robot_description}]
+    )
+    # 通过IncludeLaunchDescription包含另外一个launch文件
+    launch_gazebo = launch.actions.IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            [get_package_share_directory('gazebo_ros'), '/launch', '/gazebo.launch.py']),
+            launch_arguments=[('world',default_world_path),('verbose','true')]
+        )
+
+    # 请求 Gazebo加载机器人
+    spawn_entity_node = launch_ros.actions.Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=['-topic','/robot_description','-entity',robot_name_in_model]
+        
+    )
+
+    # 加载并激活 fishbot_joint_state_broadcaster 控制器
+    load_joint_state_controller = launch.actions.ExecuteProcess(
+        cmd='ros2 control load_controller fishbot_joint_state_broadcaster --set-state active'.split(" "),
+        output='screen'
+    )
+
+    # 加载并激活 fishbot_effort_controller 控制器
+    load_fishbot_effort_controller = launch.actions.ExecuteProcess(
+        cmd='ros2 control load_controller fishbot_effort_controller --set-state active'.split(" "),
+        output='screen'
+    )
+    
+    # 加载并激活 fishbot_diff_drive_controller 控制器
+    action_load_diff_drive_controller = launch.actions.ExecuteProcess(
+        cmd='ros2 control load_controller fishbot_diff_drive_controller --set-state active'.split(" "),
+        output='screen'
+    )
+
+    return launch.LaunchDescription([
+        action_declare_arg_mode_path,
+        robot_state_publisher_node,
+        launch_gazebo,
+        spawn_entity_node,
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=spawn_entity_node,
+                on_exit=[load_joint_state_controller],
+            )
+        ),
+        # launch.actions.RegisterEventHandler(
+        #     event_handler=launch.event_handlers.OnProcessExit(
+        #         target_action=load_joint_state_controller,
+        #         on_exit=[load_fishbot_effort_controller],
+        #     )
+        # ),
+        launch.actions.RegisterEventHandler(
+            event_handler=launch.event_handlers.OnProcessExit(
+                target_action=load_joint_state_controller,
+                on_exit=[action_load_diff_drive_controller],
+            )
+        ),
+        
+    ])
